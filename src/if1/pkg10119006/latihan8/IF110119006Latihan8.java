7ea3221b2021fb88d757ee696be0fd7be34119f2
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-10-16 08:55:47 +0700

    fix

---
+++ src/if1/pkg10119006/latihan8/IF110119006Latihan8.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119006.latihan8;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ilham zaki
+ * Kelas    : IF1
+ * NIM      : 10119006
+ * Deskripsi Program : Hasil tampil Pecahan
+ */
+public class IF110119006Latihan8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
